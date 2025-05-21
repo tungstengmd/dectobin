a5407a57671a6913e13dcf5a488c5735f7bc9f55
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-05-21 11:52:29 +0100

    Add project files.

---
+++ dectobin/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write($"enter a number :3\n\e[92m{{{Environment.UserName}@{Environment.MachineName}}}% \e[0m");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write(Convert.ToString(num, 2));
